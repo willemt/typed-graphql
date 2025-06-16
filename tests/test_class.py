4ffa0229722f54a4800632080f834bb46608514f
Willem Thiart <himself@willemthiart.com>
2025-06-16 23:33:02 +1200

    feat: support vanilla classes

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,17 +1,10 @@
-from dataclasses import dataclass
-from functools import wraps
-from typing import Any, Generic, Iterable, List, Optional, Tuple, TypedDict, TypeVar
+from typing import List
 
 from graphql import graphql_sync
-from graphql.type import GraphQLField, GraphQLSchema, GraphQLString, GraphQLObjectType
+from graphql.type import GraphQLSchema
 
-from typed_graphql import (
-    TypedGraphqlMiddlewareManager,
-    graphql_type,
-    resolver,
-    resolverclass,
-    staticresolver,
-)
+from typed_graphql import TypedGraphqlMiddlewareManager
+from typed_graphql import graphql_type
 
 
 def test_class():
@@ -57,4 +50,24 @@ def test_snake_case():
     )
     print(result.errors)
     assert result.data == {"user": [{"myValue": "1"}]}
+    assert result.errors is None
+
+
+def test_class_with_fields():
+    class User:
+        value: str
+
+        def __init__(self, data):
+            self.value = data
+
+    class Query:
+        def resolve_user(self, info) -> List[User]:
+            return [User("1")]
+
+    assert str(graphql_type(Query).fields["user"].type) == "[User!]!"
+    schema = GraphQLSchema(query=graphql_type(Query))
+    result = graphql_sync(
+        schema, "{user { value }}", Query(), middleware=TypedGraphqlMiddlewareManager()
+    )
+    assert result.data == {"user": [{"value": "1"}]}
     assert result.errors is None
